83805ab82345647434dde7f9b492f05a3ac648d1
seregas <woldman7@gmail.com>
2017-01-21 15:23:21 +0300

    Add some params to BoxView

--- box/BoxView.php
+++ box/BoxView.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: seregas
- * Date: 19.01.17
- * Time: 18:10
- */
 
 namespace socialist\adminlte\box;
 
@@ -15,10 +9,40 @@ use Yii;
 
 class BoxView extends Widget
 {
+    /**
+     * @var string the type of box.
+     * Can be `default`, `primary`, `success`, `info`, `danger` or `warning`.
+     *
+     * Defaul value `primary`
+     */
     public $type = 'primary';
 
+    /**
+     * @var string The box background class name
+     *
+     * ```php
+     * [
+     *      'background' => 'bg-light-blue-gradient'
+     * ]
+     * ```
+     */
+    public $background;
+
+    /**
+     * @var array of header information:
+     * *title* - The title of box
+     * *icon* - The icon of box header
+     */
     public $header;
 
+    public $footer;
+
+    public $solid = false;
+
+    /**
+     * @var array of tools object parameters:
+     * *class* - Tools class path. Default \socialist\adminlte\box\Tools
+     */
     public $tools = [];
 
     public function init()
@@ -32,8 +56,14 @@ class BoxView extends Widget
     public function run()
     {
         $content = ob_get_clean();
+        $boxClasses = [
+            'box', "box-{$this->type}",
+            $this->background,
+            ($this->solid) ? 'box-solid' : ''
+        ];
+
         $body = Html::tag('div', $content, ['class' => ['box-body']]);
-        return Html::tag('div', $this->header() . $body, ['class' => 'box box-' . $this->type]);
+        return Html::tag('div', $this->header() . $body .  $this->footer(), ['class' => $boxClasses]);
     }
 
     protected function header()
@@ -47,5 +77,14 @@ class BoxView extends Widget
         $this->header['tools'] = Yii::createObject($this->tools);
 
         return $this->render('box-header', $this->header);
+    }
+
+    protected function footer()
+    {
+        if (!$this->footer) {
+            return '';
+        }
+
+        return Html::tag('div', $this->footer, ['class' => 'box-footer clearfix']);
     }
 }
